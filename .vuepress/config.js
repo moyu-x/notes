b71333d16499be3cfd29c5625877a2de2b09830d
idwangmo <idwangmo@gmail.com>
2018-08-15 16:12:38 +0800

    modify: 修改部分配置

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,11 +1,20 @@
 module.exports = {
     title: '墨语笔记补完计划',
     description: '墨语笔记补完计划',
+    serviceWorker: true,
+    evergreen: true,
     themeConfig: {
       lastUpdated: 'Last Updated',
       searchMaxSuggestions: 10,
       repo: 'idwangmo/note',
       repoLabel: 'Github',
-      docsBranch: 'vuepress'
+      docsBranch: 'vuepress',
+      siderbar: 'auto',
+      nav: [
+        { text: "主页", link: "https://www.mosdev.xyz"}
+      ]
+    },
+    markdown: {
+      lineNumbers: true
     }
   }
---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+# 保证脚本抛出错误
+set -e
+
+# 生成静态文件
+vuepress build .
